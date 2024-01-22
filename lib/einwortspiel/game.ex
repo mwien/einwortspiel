67f2ecfa9a37830d9426818a36e187ed3b1f5749
Marcel Wienöbst <marcel.wienoebst@gmx.de>
2024-01-22 13:17:02 +0100

    propose naming changes

--- lib/einwortspiel/game.ex
+++ lib/einwortspiel/game.ex
@@ -13,28 +13,15 @@ defmodule Einwortspiel.Game do
     end
   end
 
-  def has_joined?(table, player_id) do
-    Map.has_key?(table.players, player_id)
-  end
-
   # TODO: also check that table_id exists as service name and handle error?
+
+  # TODO: -> name create_player()
   def join(table_id, player_id) do
     service_name(table_id)
     |> GenServer.call({:join, player_id})
   end
 
-  # check whether new round can be started
-  def can_start_round?(table) do
-    Table.can_start_round?(table)
-  end
-
-  # possible commands: 
-  # :start -> for starting the (next) round
-  def manage_round(table_id, command, player_id) do
-    service_name(table_id)
-    |> GenServer.call({:manage_round, command, player_id})
-  end
-
+  # TODO: name update_player!
   # possible attributes: 
   # :name -> name of player
   def set_attribute(table_id, player_id, attribute, value) do
@@ -42,12 +29,32 @@ defmodule Einwortspiel.Game do
     |> GenServer.call({:set_attribute, player_id, attribute, value})
   end
 
+  # better name -> manage_game -> :start
+  # possible commands: 
+  # :start -> for starting the (next) round
+  def manage_round(table_id, command, player_id) do
+    service_name(table_id)
+    |> GenServer.call({:manage_round, command, player_id})
+  end
+
+  # TODO: maybe rename make_move
   # possible moves: 
   # {:submit_clue, clue} 
   # {:submit_guess, guess}
   def move(table_id, player_id, move) do
     service_name(table_id)
     |> GenServer.call({:move, player_id, move})
+  end
+
+  # rename to has_player?
+  def has_joined?(table, player_id) do
+    Map.has_key?(table.players, player_id)
+  end
+
+  # rename to "can_start?" ??? -> "ready_to_start?"
+  # check whether new round can be started
+  def can_start_round?(table) do
+    Table.can_start_round?(table)
   end
 
   defp service_name(table_id) do
